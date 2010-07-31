a3b1d3897fc7f47ab40ad833071daf881a02da59
Ian Dees <ian.dees@gmail.com>
2010-07-30 20:08:57 -0500

    Moved guts to a separate class.

--- src/com/yellowbkpk/geo/ImageCapture.java
+++ src/com/yellowbkpk/geo/ImageCapture.java
@@ -1,34 +1,13 @@
 package com.yellowbkpk.geo;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.List;
-
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.PixelFormat;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
-import android.hardware.Camera.Size;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.Window;
-import android.widget.Toast;
 
-public class ImageCapture extends Activity implements LocationListener {
+public class ImageCapture extends Activity {
     
-    private SurfaceView preview=null;
-    private SurfaceHolder previewHolder=null;
-    private Camera camera=null;
-    private LocationManager lm;
+    private Preview preview=null;
     
     /** Called when the activity is first created. */
     @Override
@@ -37,194 +16,33 @@ public class ImageCapture extends Activity implements LocationListener {
         
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         
-        setContentView(R.layout.main);
+        preview = new Preview(this);
+        setContentView(preview);
         
-        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Log.i("PictureDemo", "Starting up GPS location provider...");
-            startGPS();
-        }
-        
-        preview=(SurfaceView)findViewById(R.id.preview);
-        previewHolder=preview.getHolder();
-        previewHolder.addCallback(surfaceCallback);
-        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
-
-    private void startGPS() {
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                1000, 10,
-                this);
-    }
-    
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        if(LocationProvider.AVAILABLE == status) {
-            Log.i("PictureDemo", "Satellite fix.");
-            Toast
-                .makeText(ImageCapture.this, "GPS Fix", Toast.LENGTH_LONG)
-                .show();
-        }
-    }
-    public void onProviderEnabled(String provider) {
-        Log.d("PictureDemo", "Provider enabled.");
-    }
-    public void onProviderDisabled(String provider) {
-        Log.d("PictureDemo", "Provider disabled.");
-    }
-    public void onLocationChanged(Location location) {
-        Log.i("PictureDemo", "Location changed to " + location.getLatitude() + "," + location.getLongitude());
-        
-        Parameters parameters = camera.getParameters();
-        parameters.setGpsLatitude(location.getLatitude());
-        parameters.setGpsLongitude(location.getLongitude());
-        parameters.setGpsTimestamp(location.getTime());
-        camera.setParameters(parameters);
-    }
-    
-    @Override
-    protected void onPause() {
-        stopGPS();
-        
-        super.onPause();
-    }
-
-    private void stopGPS() {
-        lm.removeUpdates(this);
-    }
-
-    @Override
-    protected void onResume() {
-        startGPS();
-        
-        super.onResume();
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode==KeyEvent.KEYCODE_VOLUME_DOWN) {
-            takePicture();
+            preview.takePicture();
 
             return(true);
         }
 
         return(super.onKeyDown(keyCode, event));
     }
-
-    private void takePicture() {
-        camera.takePicture(null, null, photoCallback);
-    }
     
-    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
-        final double ASPECT_TOLERANCE = 0.05;
-        double targetRatio = (double) w / h;
-        if (sizes == null) return null;
-
-        Size optimalSize = null;
-        double minDiff = Double.MAX_VALUE;
-
-        int targetHeight = h;
-
-        // Try to find an size match aspect ratio and size
-        for (Size size : sizes) {
-            double ratio = (double) size.width / size.height;
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-            if (Math.abs(size.height - targetHeight) < minDiff) {
-                optimalSize = size;
-                minDiff = Math.abs(size.height - targetHeight);
-            }
-        }
-
-        // Cannot find the one match the aspect ratio, ignore the requirement
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE;
-            for (Size size : sizes) {
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-        }
-        return optimalSize;
+    @Override
+    protected void onPause() {
+        preview.stopGPS();
+        
+        super.onPause();
     }
 
-    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
-        public void surfaceCreated(SurfaceHolder holder) {
-            Log.i("PictureDemo", "Opening camera");
-            camera=Camera.open();
-
-            try {
-                Parameters parameters = camera.getParameters();
-//                List<Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
-//                for (Size size : supportedPreviewSizes) {
-//                    Log.i("PictureDemo", "Supported preview: " + size.width + "x" + size.height);
-//                }
-//                Log.i("PictureDemo", "Preview window: " + holder.getSurfaceFrame().width() + "x" + holder.getSurfaceFrame().height());
-//                parameters.setPreviewSize(480, 320);
-                parameters.setPictureFormat(PixelFormat.JPEG);
-                camera.setParameters(parameters);
-                camera.setPreviewDisplay(previewHolder);
-            }
-            catch (Throwable t) {
-                Log.e("PictureDemo-surfaceCallback",
-                            "Exception in setPreviewDisplay()", t);
-                Toast
-                    .makeText(ImageCapture.this, t.getMessage(), Toast.LENGTH_LONG)
-                    .show();
-            }
-        }
-
-        public void surfaceChanged(SurfaceHolder holder,
-                                     int format, int width,
-                                     int height) {
-            Log.i("PictureDemo", "Surface changed: " + width + "x" + height);
-
-            Parameters parameters = camera.getParameters();
-            List<Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
-            Size optimal = getOptimalPreviewSize(supportedPreviewSizes, width, height);
-            parameters.setPreviewSize(optimal.width, optimal.height);
-            camera.setParameters(parameters);
-            
-            camera.startPreview();
-        }
-
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            camera.stopPreview();
-            camera.release();
-            camera=null;
-        }
-    };
-
-    Camera.PictureCallback photoCallback=new Camera.PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
-            new SavePhotoTask().execute(data);
-            camera.startPreview();
-        }
-    };
-
-    class SavePhotoTask extends AsyncTask<byte[], String, String> {
-        @Override
-        protected String doInBackground(byte[]... jpeg) {
-            File photo=new File(getExternalFilesDir("photos"),
-                                                    "photo.jpg");
-//                                                    System.currentTimeMillis() + ".jpg");
-
-            if (photo.exists()) {
-                photo.delete();
-            }
-
-            try {
-                FileOutputStream fos=new FileOutputStream(photo.getPath());
-
-                fos.write(jpeg[0]);
-                fos.close();
-            }
-            catch (java.io.IOException e) {
-                Log.e("PictureDemo", "Exception in photoCallback", e);
-            }
-            
-            Log.i("PictureDemo", "Saved to " + photo.getAbsolutePath());
-
-            return(null);
-        }
+    @Override
+    protected void onResume() {
+        preview.startGPS();
+        
+        super.onResume();
     }
 }
--- src/com/yellowbkpk/geo/Preview.java
+++ src/com/yellowbkpk/geo/Preview.java
@@ -1,24 +1,30 @@
 package com.yellowbkpk.geo;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.hardware.Camera;
-import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
-class Preview extends SurfaceView implements SurfaceHolder.Callback {
-    private static final String TAG = "Preview";
-
-    SurfaceHolder mHolder;
-    public Camera camera;
+class Preview extends SurfaceView implements SurfaceHolder.Callback, LocationListener {
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private LocationManager lm;
 
     Preview(Context context) {
         super(context);
@@ -28,37 +34,24 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         mHolder = getHolder();
         mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
+        lm = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.i("PictureDemo", "Starting up GPS location provider...");
+            startGPS();
+        }
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, acquire the camera and tell it where
         // to draw.
-        camera = Camera.open();
+        mCamera = Camera.open();
         try {
-            camera.setPreviewDisplay(holder);
-
-            camera.setPreviewCallback(new PreviewCallback() {
-
-                public void onPreviewFrame(byte[] data, Camera arg1) {
-                    FileOutputStream outStream = null;
-                    try {
-                        outStream = new FileOutputStream(String.format(
-                                "/sdcard/%d.jpg", System.currentTimeMillis()));
-                        outStream.write(data);
-                        outStream.close();
-                        Log.d(TAG, "onPreviewFrame - wrote bytes: "
-                                + data.length);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                    }
-                    Preview.this.invalidate();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+           mCamera.setPreviewDisplay(holder);
+        } catch (IOException exception) {
+            mCamera.release();
+            mCamera = null;
+            // TODO: add more exception handling logic here
         }
     }
 
@@ -66,25 +59,128 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         // Surface will be destroyed when we return, so stop the preview.
         // Because the CameraDevice object is not a shared resource, it's very
         // important to release it when the activity is paused.
-        camera.stopPreview();
-        camera = null;
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
+    
+    private Size getOptimalPreviewSize(List<Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.05;
+        double targetRatio = (double) w / h;
+        if (sizes == null) return null;
+
+        Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // Now that the size is known, set up the camera parameters and begin
         // the preview.
-        Camera.Parameters parameters = camera.getParameters();
-        parameters.setPreviewSize(w, h);
-        camera.setParameters(parameters);
-        camera.startPreview();
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        List<Size> sizes = parameters.getSupportedPreviewSizes();
+        Size optimalSize = getOptimalPreviewSize(sizes, w, h);
+        parameters.setPreviewSize(optimalSize.width, optimalSize.height);
+
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
     }
 
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        Paint p = new Paint(Color.RED);
-        Log.d(TAG, "draw");
-        canvas.drawText("PREVIEW", canvas.getWidth() / 2,
-                canvas.getHeight() / 2, p);
+    void takePicture() {
+        mCamera.takePicture(null, null, photoCallback);
     }
+    
+    Camera.PictureCallback photoCallback=new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            new SavePhotoTask().execute(data);
+            camera.startPreview();
+        }
+    };
+
+    class SavePhotoTask extends AsyncTask<byte[], String, String> {
+        @Override
+        protected String doInBackground(byte[]... jpeg) {
+            File photo=new File(getContext().getExternalFilesDir("photos"),
+                                                    "photo.jpg");
+//                                                    System.currentTimeMillis() + ".jpg");
+
+            if (photo.exists()) {
+                photo.delete();
+            }
+
+            try {
+                FileOutputStream fos=new FileOutputStream(photo.getPath());
+
+                fos.write(jpeg[0]);
+                fos.close();
+            }
+            catch (java.io.IOException e) {
+                Log.e("PictureDemo", "Exception in photoCallback", e);
+            }
+            
+            Log.i("PictureDemo", "Saved to " + photo.getAbsolutePath());
+
+            return(null);
+        }
+    }
+    
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        if(LocationProvider.AVAILABLE == status) {
+            Log.i("PictureDemo", "Satellite fix.");
+            Toast
+                .makeText(getContext(), "GPS Fix", Toast.LENGTH_LONG)
+                .show();
+        }
+    }
+    public void onProviderEnabled(String provider) {
+        Log.d("PictureDemo", "Provider enabled.");
+    }
+    public void onProviderDisabled(String provider) {
+        Log.d("PictureDemo", "Provider disabled.");
+    }
+    public void onLocationChanged(Location location) {
+        Log.i("PictureDemo", "Location changed to " + location.getLatitude() + "," + location.getLongitude());
+        
+        Parameters parameters = mCamera.getParameters();
+        parameters.setGpsLatitude(location.getLatitude());
+        parameters.setGpsLongitude(location.getLongitude());
+        parameters.setGpsTimestamp(location.getTime());
+        mCamera.setParameters(parameters);
+    }
+    
+
+    void startGPS() {
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, 10,
+                this);
+    }
+
+    void stopGPS() {
+        lm.removeUpdates(this);
+    }
+
 }
